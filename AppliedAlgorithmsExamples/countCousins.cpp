ad28055a2f76054b7e5c7fb3a6d0535cbf5626fb
Caleb Herring <cherring1@harding.edu>
2018-09-04 12:33:52 -0500

    fdfs

--- AppliedAlgorithmsExamples/countCousins.cpp
+++ AppliedAlgorithmsExamples/countCousins.cpp
@@ -1,1 +1,31 @@
 //countYourCousins example
+
+/*
+  5         10      15 16 17            25 26 27 28             90
+
+  10 is a child of 5
+  15 16 17 are children of 10
+  25 26 27 28 are children of 15
+  90 is child of 16
+
+  25,26,27,28 and 90 are Cousins
+
+cousins are same grandparent and different parents
+
+hint:
+  create a vector
+      for every node record
+              record grandparent and parent
+
+      grandparent - parent[parent]
+
+
+5 and 10 do not have grandparents  && 5 doesnt have a parent
+
+
+
+
+
+
+
+*/
