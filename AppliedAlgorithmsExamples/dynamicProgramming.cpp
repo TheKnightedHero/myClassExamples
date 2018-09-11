eb7ab6f9bc8d6be5e5e218a9ebb58d9c0756e492
Caleb Herring <cherring1@harding.edu>
2018-09-11 12:05:25 -0500

    created new example

---
+++ AppliedAlgorithmsExamples/dynamicProgramming.cpp
@@ -0,0 +1,7 @@
+/*0-1 knnapsack problem using dynamic programming
+
+profit[i][wt] - maximum profit if we can choose from the first i items but are restricted to a maximum weight (wt)
+
+weight[i] - weight of item i
+item[i] - value of item i 
+*/
