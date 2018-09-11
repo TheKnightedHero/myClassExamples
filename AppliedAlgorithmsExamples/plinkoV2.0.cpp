6822c2a1fc98271c03cfb1e2d0e4b9cbb979ecbe
Caleb Herring <cherring1@harding.edu>
2018-09-11 12:46:44 -0500

    jkkghj

--- AppliedAlgorithmsExamples/plinkoV2.0.cpp
+++ AppliedAlgorithmsExamples/plinkoV2.0.cpp
@@ -4,6 +4,10 @@
 
   so a game of plinko with 5 rows, there are 15 paths
 
-  
+  of maximum, what were the decisions made to get to that value?
+
+  Node
+  (value, MAxSum, StoredAnswerToQuestion(option1,option2))
+  (20, 37, 'L')
 
 */
