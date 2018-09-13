7492aeed984c299fc98c416d3b10383d39bfc610
Caleb Herring <cherring1@harding.edu>
2018-09-13 11:45:55 -0500

    add hints for count cousins problem

--- AppliedAlgorithmsExamples/countCousins.cpp
+++ AppliedAlgorithmsExamples/countCousins.cpp
@@ -25,7 +25,35 @@ hint:
 
 
 
+2 parallel arrays
+currentParentsINdex = 0;  //put into parents array
+
+value[]
+0        1
+1        3          currentParentsIndex = 1
+2        4
+3        5
+4        8          currentParentsIndex = 2
+5        9
+6       15         currentParentsIndex = 3
+7       30
+8       31
+9
 
 
+parent[]
+-1            corresponds with value array
+0
+0
+0
+1
+1
+2
+3
+3
 
+grandparent = parent[parent[i]];
+count all values where grands are equal but parents are not
+
+if statement to make sure -1 isnt read or will be out of the array
 */
