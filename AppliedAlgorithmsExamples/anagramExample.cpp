d46738c2e1560478b314dfa59f52e8deb5095293
Caleb Herring <cherring1@harding.edu>
2018-09-13 12:11:46 -0500

    more answer code

--- AppliedAlgorithmsExamples/anagramExample.cpp
+++ AppliedAlgorithmsExamples/anagramExample.cpp
@@ -32,3 +32,84 @@ use  isAlpha()  && toUpper
 /*
   Below are in class examples
 */
+void main()
+{
+  ifstream fin("anagrams.in");
+  if(fin.is_open())
+  {
+    ofstream fout("anagrams.out");
+    string line1, line2;
+
+    getline(fin, line1);
+    getline(fin, line2);
+    while(!(line == "<end>" && line2 == "<end"))
+    {
+      //do stuff
+      vector<int> coutLine1Letters (26,0);  //26 in vector, intialized to 0
+      vector<int> coutLine2Letters (26, 0);
+
+      //count # of A's, B's, C's, etc in line 1
+      for each(char c in line1)
+      {
+        if(isalpha(c))
+        {
+          int letterIndex = tolower(c) - 'a';
+          coutLine1Letters[letterIndex]++;
+        }
+      }
+
+      for each(char c in line2)
+      {
+        if(isalpha(c))
+        {
+          int letterIndex = tolower(c) - 'a';
+          coutLine2Letters[letterIndex]++;
+        }
+      }
+
+      if(coutLine1Letters == coutLine2Letters)
+      {
+        fout << "YES";
+      }
+      else
+      {
+        fout << "NO"
+      }
+
+      getline(fin, line1);
+      getline(fin, line2);
+
+    }
+    fout.close();
+    fin.close();
+  }
+  else
+  {
+    cout << "unable to open";
+  }
+}
+
+/*
+Alternate code for void main()
+{
+  ifstream fin("anagrams.in");
+  if(fin.is_open())
+  {
+    ofstream fout("anagrams.out");
+    string line1, line2;
+
+    getline(fin, line1);
+    getline(fin, line2);
+    while(!(line == "<end>" && line2 == "<end"))
+    {
+    
+    fout.close();
+    fin.close();
+  }
+  else
+  {
+    cout << "unable to open";
+  }
+
+}
+*/
