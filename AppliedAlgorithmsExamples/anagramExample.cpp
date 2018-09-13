fecfe7956eb11b13f9697d1eb875b00fa9473743
Caleb Herring <cherring1@harding.edu>
2018-09-13 11:55:10 -0500

    update on anagrams

--- AppliedAlgorithmsExamples/anagramExample.cpp
+++ AppliedAlgorithmsExamples/anagramExample.cpp
@@ -1,6 +1,9 @@
 //Anagram class example
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
 
@@ -9,17 +12,23 @@ using namespace std;
   check for alpha character if its not alphabetical, skip it
   if it is, add 1 to element in an array
 
-*/
+
 
 int letterCount1[26];
 int letterCount2[26];
 
 use  isAlpha()  && toUpper
 
-/*
+
   if letter 'B'
   ('B' - 65)     ==  (66 - 65)      65 is the ASCII for 'A'
 
   compare the integer arrays
   look at multiset if you want to be fancy
+*/
+
+
+
+/*
+  Below are in class examples
 */
