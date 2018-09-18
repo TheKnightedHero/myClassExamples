dc4f95419b2dda0241aed648100c11ef4a1cf317
Caleb Herring <cherring1@harding.edu>
2018-09-18 12:28:51 -0500

    asdf

---
+++ AppliedAlgorithmsExamples/brokenClock.cpp
@@ -0,0 +1,12 @@
+//create bitsets to solve problem
+
+bitset<8> three("11011010");
+
+//do this for 1-9
+
+
+
+bitset<8> working("01101110")  //LED 4 is broken
+
+
+bitset<8> result = three & working;  //we and these together
