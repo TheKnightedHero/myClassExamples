4494f12e048da46a05a963a13fbfd8d100d66e7e
Caleb Herring <cherring1@harding.edu>
2018-10-03 11:29:32 -0500

    push files perl

--- Operating Systems/PerlProject.pl
+++ Operating Systems/PerlProject.pl
@@ -1,1 +1,22 @@
 #!/usr/bin/perl -w
+use strict;
+use File::copy
+
+#work on this pseudo code to complete the project
+
+my($sourceFile);
+my ($inputFile);
+my($outputFile);
+
+mkdir("./scratch");
+
+$sourceFile = ARGV[0];
+$inputFile = ARGV[1];
+$outputFile = ARGV[2];
+
+copy($sourceFile, "./scratch/hello.cpp");
+copy($inputFile, "./scratch/.");
+copy($outputFile, "./scratch/expectedHello.out");
+
+chdir("./scratch");
+unlink("*");
