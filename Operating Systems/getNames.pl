008487c95373e9c5857eb6059010f6875afb366f
Caleb Herring <cherring1@harding.edu>
2018-11-04 17:50:51 -0600

    push PERL project#2

---
+++ Operating Systems/getNames.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+use strict;
+
+# Caleb Herring
+
+my ($line, $word, %wordCount);
+
+while ($line = <STDIN>)
+{
+	chomp $line;
+	
+	my @words = split(":", $line);
+	foreach $word (@words)
+	{
+		
+	}
+	
+}
