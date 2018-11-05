376a148dbb04a4147a9c710b32d4e1a6e26b51fc
Caleb Herring <cherring1@harding.edu>
2018-11-04 19:23:02 -0600

    push completed project

--- Operating Systems/getNames.pl
+++ Operating Systems/getNames.pl
@@ -3,16 +3,37 @@ use strict;
 
 # Caleb Herring
 
-my ($line, $word, %wordCount);
+my ($line, @namesArray, %wordCount, $username, $password, $UID, $GID, $actualName, @everythingElse);
 
 while ($line = <STDIN>)
 {
 	chomp $line;
-	
-	my @words = split(":", $line);
-	foreach $word (@words)
+
+	# my @words = split(":", $line);
+	($username, $password, $UID, $GID, $actualName, @everythingElse) = split(":", $line);
+
+
+	if($GID >= 1000)
 	{
-		
+		@namesArray = split(" ", $actualName);
+		if($namesArray[0] !~ /^(Dr|Mr|Ms|Mrs|Coach)$/)
+		{
+			$actualName = $namesArray[0];
+		}
+		else
+		{
+			$actualName = $namesArray[1];
+		}
+		# $wordCount{$word} = defined($wordCount{$word}) ? $wordCount{$word} + 1 : 1;
+		$wordCount{$actualName} = defined($wordCount{$actualName}) ? $wordCount{$actualName} + 1 : 1;
 	}
-	
+}
+
+foreach $actualName (sort(keys(%wordCount)) )
+{
+	if($wordCount{$actualName} > 1)
+	{
+			#print "Test statement2";
+			print "$actualName occurs $wordCount{$actualName} times\n";
+	}
 }
